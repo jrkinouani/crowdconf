4a71e0f8c7416fc895f5feae7bfeb44b8d5acd9f
jrkinouani <kinouani@gmail.com>
2016-08-29 13:41:09 +0400

    static-pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/info'
+
+  get 'static_pages/thanks'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get info" do
+    get static_pages_info_url
+    assert_response :success
+  end
+
+  test "should get thanks" do
+    get static_pages_thanks_url
+    assert_response :success
+  end
+
+end
